7ee5dca8c28e35298db182502dfb9b0708026dcc
杨笑影 <yangwei@yangweidemac.local>
2016-05-27 20:45:31 -0700

    Init Udacity beginning-iso-pirate-fleet project.

---
+++ Pirate Fleet/ControlCenter.swift
@@ -0,0 +1,34 @@
+//
+//  ControlCenter.swift
+//  Pirate Fleet
+//
+//  Created by Jarrod Parkes on 9/2/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+struct GridLocation {
+    let x: Int
+    let y: Int
+}
+
+struct Ship {
+    let length: Int
+    let location: GridLocation
+    let isVertical: Bool
+}
+
+class ControlCenter {
+    
+    func addShipsAndMines(human: Human) {
+        <#WRITE YOUR CODE HERE!#>
+    }
+    
+    func calculateFinalScore(gameStats: GameStats) -> Int {
+        
+        var finalScore: Int
+        
+        finalScore = 0                
+        
+        return finalScore
+    }
+}
